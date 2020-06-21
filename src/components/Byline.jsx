3035e7110b710be74272bac47022402f5b59278f
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-21 19:35:31 +0700

    View

---
+++ src/components/Byline.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import SmallText from './SmallText';
+import * as globalStyles from '../styles/global';
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  location: {
+    color: globalStyles.MUTED_COLOR,
+  },
+});
+
+const Byline = ({ date, author, location }) => (
+  <View>
+    <View style={styles.row}>
+      <SmallText>{date.toLocaleDateString()}</SmallText>
+      <SmallText>{author}</SmallText>
+    </View>
+
+    {location ? (
+      <View style={styles.row}>
+        <SmallText style={styles.location}>
+          {location}
+        </SmallText>
+      </View>
+    ) : null}
+  </View>
+);
+
+export default Byline;
+
+Byline.propTypes = {
+  author: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  location: PropTypes.string.isRequired,
+};
